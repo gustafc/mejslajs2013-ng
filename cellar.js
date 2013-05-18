a64e47f47057d57e8b9aba38508c5a85dea4ca2e
Gustaf Cele <gustaf.cele@mejsla.se>
2013-05-18 13:56:25 +0200

    Added basic functionality

---
+++ cellar.js
@@ -0,0 +1,13 @@
+﻿ function CellarCtrl($scope) {
+	$scope.newWine = {};
+	$scope.wines = [
+		{name: "Henri Goutorbe Special Club", vintage: 2002},
+		{name: "Krug Grand Cuvée", vintage: undefined},
+		{name: "Recaredo Gran Reserva Brut Nature", vintage: 2006},
+		{name: "Françoise Bedel Entre Ciel et Terre", vintage: undefined},
+	 ];
+	 $scope.add = function addWine(){
+		 $scope.wines.push($scope.newWine);
+		 $scope.newWine = {};
+	 }
+ }
