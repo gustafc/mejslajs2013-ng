8563686146944575cb72e4218ca71359c04b8e85
Gustaf Cele <gustaf.cele@mejsla.se>
2013-05-22 22:12:10 +0200

    Added varietal, though not editable yet

--- cellar.js
+++ cellar.js
@@ -5,15 +5,18 @@
 		return index !== -1;
 	}
 	var winesInEditMode = [];
+	
 	$scope.newWine = {};
 	$scope.wines = [
-		{name: "Henri Goutorbe Special Club", vintage: 2002},
-		{name: "Krug Grand Cuvée", vintage: null},
-		{name: "Recaredo Gran Reserva Brut Nature", vintage: 2006},
-		{name: "Françoise Bedel Entre Ciel et Terre", vintage: null},
+		{name: "Henri Goutorbe Special Club", vintage: 2002, grapes: ["chardonnay", "pinot meunier", "pinot noir"]},
+		{name: "Krug Grand Cuvée", vintage: null, grapes: []},
+		{name: "Recaredo Gran Reserva Brut Nature", vintage: 2006, grapes: ["macabeo", "parellada", "xarel·lo"]},
+		{name: "Françoise Bedel Entre Ciel et Terre", vintage: null, grapes: ["pinot meunier"]},
 	 ];
 	 $scope.add = function addWine(){
 		$scope.newWine.vintage = parseInt($scope.newWine.vintage, 10) || null;
+		$scope.newWine.grapes = $scope.newWine.grapes.replace(/\s+/, " ").toLowerCase().split(/\s*,\s*/);
+		$scope.newWine.grapes.sort();
 		$scope.wines.unshift($scope.newWine);
 		$scope.newWine = {};
 	 };
@@ -26,7 +29,19 @@
 			var na = num(a), nb = num(b);
 			return na == nb ? 0 : na < nb ? -1 : 1;
 		}
-		var cmpTable = { "name": cmpString, "vintage": cmpNum };
+		function cmpArray(elementCmp){
+			return function cmpArrayImpl(a, b) {
+				var i;
+				for (i = 0; i < a.length && i < b.length; i++) {
+					var cmp = elementCmp(a[i], b[i]);
+					if (cmp != 0) return cmp;
+				}
+				// All elements equal for the length of a. If the arrays are of the same length, they're equal, else 
+				// the shorter is considered smaller.
+				return cmpNum(a.length, b.length); 
+			};
+		}
+		var cmpTable = { "name": cmpString, "vintage": cmpNum, "grapes": cmpArray(cmpString) };
 		var direction = sortDirections[sortField] || 1;
 		sortDirections[sortField] = -direction;
 		$scope.wines.sort(function(a, b){
